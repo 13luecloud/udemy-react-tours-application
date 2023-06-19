1e4fdc2265e77ca2b0e7f4a75dfa1c63d2295eb0
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2023-06-19 17:43:07 +0800

    Created Tour component

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -20,17 +20,22 @@ export default function Tours() {
 
     if (tours.length === 0) {
         return (
-            <div className="title">
-                <h2> No More Tours Left </h2>
-                <button type="button" className="btn" onClick={() => fetchTours()}> Refresh List </button>
-            </div>
+            <main>
+                <div className="title">
+                    <h2> No More Tours Left </h2>
+                    <button type="button" className="btn" onClick={() => fetchTours()}> Refresh List </button>
+                </div>
+            </main>
         )
     } 
 
     return (
-        <div className="title"> 
-            <h2> Our Tours </h2>
-            <div className="title-underline"> </div>
-        </div>
+        <main>
+            <div className="title"> 
+                <h2> Our Tours </h2>
+                <div className="title-underline"> </div>
+            </div>
+        </main>
+        
     )
 }
